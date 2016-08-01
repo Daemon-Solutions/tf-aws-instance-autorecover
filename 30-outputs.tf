160901b1d2e90a4ef1e05bfdf181389fdfa8bf02
Liam Bennett <liam@bashton.com>
2016-08-01 21:56:48 +0100

    Adding public_ip output.

--- 30-outputs.tf
+++ 30-outputs.tf
@@ -3,6 +3,10 @@ output "instance_id" {
   value = "${aws_instance.recoverable_instance.id}"
 }
 
+output "public_ip" {
+  value = "${aws_instance.recoverable_instance.public_ip}"
+}
+
 output "availability_zone" {
   value = "${aws_instance.recoverable_instance.availability_zone}"
 }
